97c969bfd8ee1ca2451530278c488978396097ee
evdkv <71773269+evdkv@users.noreply.github.com>
2023-05-02 23:39:26 -0400

    Update model scripts

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -1,4 +1,26 @@
+import joblib
+
 # def display_topics(model, feature_names, no_top_words):
 #     for topic_idx, topic in enumerate(model.components_):
 #         print("Topic %d:" % (topic_idx))
 #         print(" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]]))
+
+# topic_word_list = []
+# def get_topics(components, feat): 
+#     for i, comp in enumerate(components):
+#         terms_comp = zip(feat,comp)
+#         sorted_terms = sorted(terms_comp, key= lambda x:x[1], reverse=True)[:7]
+#         topic=" "
+#         for t in sorted_terms:
+#             topic = topic + ' ' + t[0]
+#         topic_word_list.append(topic)
+#         print(topic_word_list)
+#     return topic_word_list
+
+# def main():
+#     model = joblib.load('models/nmf.joblib')
+#     features = joblib.load('models/tfidf_features.sav')
+#     # print(model.get_feature_names_out())
+
+#     get_topics(model.components_, features.get_feature_names_out())
+# main()
---
+++ src/models/fit_models.py
@@ -0,0 +1,52 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.decomposition import NMF, PCA
+from sklearn.manifold import TSNE
+import pandas as pd
+import joblib
+
+
+def main():
+    df = pd.read_csv('data/travel_processed.csv')
+    tfidf = extract_features(df['remarks'])
+    models = fit_models(tfidf)
+    save_models(models[0], models[1], models[2], models[3])
+
+def extract_features(df):   
+
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.6, min_df=2, stop_words='english')
+    tfidf = tfidf_vectorizer.fit_transform(df)
+
+    joblib.dump(tfidf, 'models/' + 'tfidf_features.joblib')
+    # tfidf_feature_names = tfidf_vectorizer.get_feature_names_out()
+
+    # tf_vectorizer = CountVectorizer(max_df=0.7, min_df=5, stop_words='english')
+    # tf = tf_vectorizer.fit_transform(df)
+    # tf_feature_names = tf_vectorizer.get_feature_names_out()
+
+    return tfidf
+
+def fit_models(tfidf):
+    no_topics = 10
+    no_clusters = 7
+
+    # Run NMF (topics)
+    nmf = NMF(n_components=no_topics, init='nndsvd').fit(tfidf)
+
+    # Run t-SNE (dimensionality reduction)
+    tsne = TSNE(n_components=2, init='random').fit_transform(tfidf)
+
+    kmeans = KMeans(n_clusters=no_clusters, n_init=20).fit_predict(tfidf)
+
+    pca = PCA(n_components=3).fit_transform(tfidf.toarray())
+
+    return nmf, tsne, kmeans, pca
+
+def save_models(nmf, tsne, kmeans, pca):
+    joblib.dump(nmf, 'models/' + 'nmf.joblib')
+    joblib.dump(tsne, 'models/' + 'tsne.joblib')
+    joblib.dump(kmeans, 'models/' + 'kmeans.joblib')
+    joblib.dump(pca, 'models/' + 'pca.joblib')
+
+if __name__ == '__main__':
+    main()
